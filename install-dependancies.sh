44859aefd01e61343238af50618d1ce0ae35df2e
H <hosalmeer@gmail.com>
2017-02-19 09:11:48 +0000

    Added upgrade pip from inside pip since ubuntu version is old

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -2,6 +2,7 @@
 
 sudo apt install python-pip
 sudo -H pip install --upgrade google-api-python-client
+sudo -H pip install --upgrade pip
 sudo -H pip install biopython
 sudo -H pip install Oauth2
 sudo -H pip install oauth2client
